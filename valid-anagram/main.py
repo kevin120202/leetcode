d086e497492639b2b1853150f322fa2630d8e6c4
kevin120202 <kevindelapaz21@gmail.com>
2024-06-03 22:49:36 -0500

    did problem anagram

---
+++ valid-anagram/main.py
@@ -0,0 +1,12 @@
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+    counter = {}
+    for char in s:
+        counter[char] = counter.get(char, 0) + 1
+    for char in t:
+        if char not in counter or counter[char] == 0:
+            return False
+        counter[char] -= 1
+
+    return True
